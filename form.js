552b3db60478c10a3cb3dc5f6a3822e83226d45f
Patrick Neel <patrick.neel@colorado.edu>
2017-08-30 15:59:14 -0600

    fixed read button background color and buttons

--- form.js
+++ form.js
@@ -2,7 +2,7 @@
 // document.querySelector('.right').addEventListener('click' , '.delete', userDeleteBtn);
 $('.enter-btn').on('click', userEnterBtn);
 $('.right').on('click', '.delete', userDeleteBtn);
-$('.right').on('click','.read', activateRead);
+$('.right').on('click','.read-button', activateRead);
 
 function userEnterBtn(e){
   e.preventDefault(); 
@@ -17,7 +17,7 @@ function userEnterBtn(e){
       `<article class="bookmark">
         <h2>${title}</h2> <hr />
         <a href= 'http://${url}' target='_blank'>${url}</a> <hr />
-        <button class="read">Read</button>
+        <button class="read-button">Read</button>
         <button class="delete">Delete</button>
       </article>`)
 }
@@ -29,8 +29,21 @@ function userDeleteBtn(e) {
 
 function activateRead(e){
   e.preventDefault();
-  $(this).parents('.bookmark').toggleClass('read-btn')
+  $(this).parents('.bookmark').toggleClass('read-btn');
+  $(this).closest('.read-button').toggleClass('read');
 }
 
+$('.enter-btn').on('click', function(){
+  var bookCount = $('.bookmark').length;
+  $('#counter').text("There are " + bookCount + " total bookmarks.");
+});
+
+$('.read-btn').on('click', function(){
+  var readCount = $('.read-btn').length;
+  $('#readcount').text("There are " + readCount + " total read bookmarks.")
+  console.log('idiot')
+});
+
+  
 
 
